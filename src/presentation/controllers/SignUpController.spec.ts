c389aba42f70a2937fce50af3d524946aaabb78b
christiangns <christian.santos@focustextil.com.br>
2021-10-30 19:11:56 -0300

    feat: enrure SignUpController return an error if no name is provided

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -14,4 +14,18 @@ describe('SignUpController', () => {
     expect(httpRequest.statusCode).toBe(400)
     expect(httpRequest.body).toEqual(new Error('Mising params: name'))
   })
+
+  test('should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const hhtRequest = {
+      body: {
+        name: 'any',
+        password: 'password',
+        passwordConfirm: 'password'
+      }
+    }
+    const httpRequest = sut.handle(hhtRequest)
+    expect(httpRequest.statusCode).toBe(400)
+    expect(httpRequest.body).toEqual(new Error('Mising params: email'))
+  })
 })
