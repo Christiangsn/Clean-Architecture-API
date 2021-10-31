e16d17c05cd66fac7ac69d4e94e588ab70216254
christiangns <christian.santos@focustextil.com.br>
2021-10-31 00:09:56 -0300

    refactor: move sute creation to a factory helper method

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -1,9 +1,13 @@
 import { SignUpController } from './SignUp'
 import { MissingParamError } from '../errors/MissingParamsError'
 
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
 describe('SignUpController', () => {
   test('should return 400 if no email is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const hhtRequest = {
       body: {
         name: 'any',
@@ -17,7 +21,7 @@ describe('SignUpController', () => {
   })
 
   test('should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const hhtRequest = {
       body: {
         email: 'any',
@@ -31,7 +35,7 @@ describe('SignUpController', () => {
   })
 
   test('should return 400 if no password is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const hhtRequest = {
       body: {
         name: 'any',
