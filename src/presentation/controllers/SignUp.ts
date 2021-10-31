cc6b5cd4c91e5576884d9ea421b48c43868c0cfa
christiangns <christian.santos@focustextil.com.br>
2021-10-31 01:33:23 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -1,10 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocol/http'
 import { IMissingParamError } from '../errors/MissingParamsError'
-import { badRequest } from '../helpers/httpHelper'
+import { badRequest, serverError } from '../helpers/httpHelper'
 import { ProtocolControllers } from '../protocol/controller'
 import { IInvalidParamsError } from '../errors/InvalidParamsError'
 import { IEmailValidator } from '../protocol/emailValidator'
-import { IServerError } from '../errors/serverError'
 
 export class SignUpController implements ProtocolControllers {
   constructor (private emailValidator: IEmailValidator) {}
@@ -23,10 +22,7 @@ export class SignUpController implements ProtocolControllers {
         return badRequest(new IInvalidParamsError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new IServerError()
-      }
+      return serverError()
     }
   }
 }
--- src/presentation/helpers/httpHelper.ts
+++ src/presentation/helpers/httpHelper.ts
@@ -1,6 +1,12 @@
+import { IServerError } from '../errors/serverError'
 import { HttpResponse } from '../protocol/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
+})
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new IServerError()
 })
