44225274527b50123ca865287f752b60f576e7eb
Christiangsn <christianguimaraes1996@gmail.com>
2021-11-02 07:44:47 -0300

    test: ensure AccountPrismRepoitory returns an account on success

--- src/infra/database/Prisma/AccountRepository/Account.ts
+++ src/infra/database/Prisma/AccountRepository/Account.ts
@@ -1,10 +1,13 @@
 import { AddAccountRepository } from '../../../../data/protocols/addAccountRepository'
 import { AccountModel } from '../../../../domain/entities/Account'
 import { AddAccountModel } from '../../../../domain/useCases/AddAccount'
-import { PrismaHelper } from '../helpers/prismaHelpers'
+import db from '../helpers/prismaHelpers'
 
 export class AccountPrismaRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    return new Promise<AccountModel>((resolve, reject) => { resolve(null) })
+    const resultAccount = await db.user.create({
+      data: accountData
+    })
+    return resultAccount
   }
 }
