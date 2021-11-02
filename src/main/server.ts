85b6d8e99505e9256a1f3ab0be54b95f349fd5ee
Christiangsn <christianguimaraes1996@gmail.com>
2021-11-02 16:23:03 -0300

    feat: connect to a prisma before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,10 @@
-import app from './app'
+import db from '@infra/database/Prisma/helpers/prismaHelpers'
 
-app.listen(process.env.APP_PORT, () => {
-  console.log('Server running at http://localhost:5000')
-})
+db.$connect()
+  .then(async () => {
+    const app = (await import('./app')).default
+    app.listen(process.env.APP_PORT, () => {
+      console.log(`Server running at http://localhost:${process.env.APP_PORT}`)
+    })
+  })
+  .catch(console.error)
