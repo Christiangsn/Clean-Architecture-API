61c4037326043021264f36902ebdd241c9fa09c6
Christiangsn <christianguimaraes1996@gmail.com>
2021-11-02 07:44:31 -0300

    feat: ensure AccountPrismRepoitory returns an account on success

---
+++ src/infra/database/Prisma/AccountRepository/Account.spec.ts
@@ -0,0 +1,26 @@
+import db from '../helpers/prismaHelpers'
+import { AccountPrismaRepository } from './Account'
+
+describe('Account SqLite Repository', () => {
+  beforeAll(async () => {
+    db.$connect()
+  })
+
+  afterAll(async () => {
+    db.$disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountPrismaRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@email.com')
+    expect(account.password).toBe('any_password')
+  })
+})
