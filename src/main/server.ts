0073966fb59f3fa835fe8b96552493f59543803b
christiangsn <christianguimaraes1996@gmail.com>
2021-11-07 21:22:55 -0300

    refactor: starting server on database for prisma

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,8 @@
-import db from '@infra/database/Prisma/helpers/prismaHelpers'
+import { Prisma } from '@infra/database/Prisma/helpers/prismaHelpers'
 
-db.$connect()
+Prisma.connection()
   .then(async () => {
+    console.log('Database is running...')
     const app = (await import('./app')).default
     app.listen(process.env.APP_PORT, () => {
       console.log(`Server running at http://localhost:${process.env.APP_PORT}`)
