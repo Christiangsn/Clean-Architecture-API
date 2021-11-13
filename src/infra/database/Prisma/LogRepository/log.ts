14e17e3491624b8573a9c911cdb6779c9a94d3bc
christiangsn <christianguimaraes1996@gmail.com>
2021-11-12 23:45:13 -0300

    feat: add column table of the logError prisma and generation

--- src/infra/database/Prisma/LogRepository/log.ts
+++ src/infra/database/Prisma/LogRepository/log.ts
@@ -3,7 +3,6 @@ import { Prisma } from '../helpers/prismaHelpers'
 
 class LogPrismaRepository implements LogErrorRepository {
   async logError (stackError: string): Promise<void> {
-    console.log('entrou aqui')
     await Prisma.client.logsError.create({
       data: {
         stack: stackError,
