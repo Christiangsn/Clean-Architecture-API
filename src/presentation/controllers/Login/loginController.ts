9a11b0393f0f8f46647ac8b13c8df2820b81944f
christiangsn <christianguimaraes1996@gmail.com>
2021-11-13 16:20:12 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/Login/loginController.ts
+++ src/presentation/controllers/Login/loginController.ts
@@ -1,5 +1,5 @@
 import { IInvalidParamsError, IMissingParamError } from '@presentation/errors'
-import { badRequest } from '@presentation/helpers/httpHelper'
+import { badRequest, serverError } from '@presentation/helpers/httpHelper'
 import { HttpRequest, HttpResponse, ProtocolControllers } from '../../protocol'
 import { IEmailValidator } from '../SignUp/SignUpProtocols'
 
@@ -9,18 +9,22 @@ class LoginController implements ProtocolControllers {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
+    try {
+      const { email, password } = httpRequest.body
 
-    if (!email) {
-      return new Promise(resolve => resolve(badRequest(new IMissingParamError('email'))))
-    }
+      if (!email) {
+        return new Promise(resolve => resolve(badRequest(new IMissingParamError('email'))))
+      }
 
-    if (!password) {
-      return new Promise(resolve => resolve(badRequest(new IMissingParamError('password'))))
-    }
+      if (!password) {
+        return new Promise(resolve => resolve(badRequest(new IMissingParamError('password'))))
+      }
 
-    if (this.emailValidator.isValid(email)) {
-      return new Promise(resolve => resolve(badRequest(new IInvalidParamsError('email'))))
+      if (!this.emailValidator.isValid(email)) {
+        return new Promise(resolve => resolve(badRequest(new IInvalidParamsError('email'))))
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
