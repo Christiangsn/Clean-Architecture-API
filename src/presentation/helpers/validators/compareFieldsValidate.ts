e0be6fd385f3e9b4d89d54fdff53729e71be51ef
christiangsn <christianguimaraes1996@gmail.com>
2021-11-14 00:07:26 -0300

    refactor: ensure params Request for SignUpControllers

---
+++ src/presentation/helpers/validators/compareFieldsValidate.ts
@@ -0,0 +1,15 @@
+import { IInvalidParamsError } from '@presentation/errors'
+import { IValidation } from './validation'
+
+export class CompareFieldValidate implements IValidation {
+  constructor (
+    private fieldName: string,
+    private fieldCompareName: string
+  ) { }
+
+  validate (input: any): Error {
+    if (!input(this.fieldName) !== input(this.fieldCompareName)) {
+      return new IInvalidParamsError(this.fieldCompareName)
+    }
+  }
+}
