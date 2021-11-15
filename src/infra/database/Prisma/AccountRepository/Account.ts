f9277772b850329d45cdaa7c44e995aa603df546
christiangsn <christianguimaraes1996@gmail.com>
2021-11-15 16:17:57 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/database/Prisma/AccountRepository/Account.ts
+++ src/infra/database/Prisma/AccountRepository/Account.ts
@@ -3,8 +3,9 @@ import { AccountModel } from '@domain/contracts/addAccount'
 import { AddAccountModel } from '../../../../domain/contracts/addAccount'
 import { AddAccountFactory } from '@domain/factories/addAccount'
 import { Prisma } from '../helpers/prismaHelpers'
+import { loadAccountByEmailRepository } from '@data/protocols/database/loadAccountByEmailRepository'
 
-export class AccountPrismaRepository implements AddAccountRepository {
+export class AccountPrismaRepository implements AddAccountRepository, loadAccountByEmailRepository {
   constructor (private accountFactory: AddAccountFactory) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
@@ -13,5 +14,12 @@ export class AccountPrismaRepository implements AddAccountRepository {
     })
     const account = this.accountFactory.addFactory(resultAccount)
     return account
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const findAccount = await Prisma.client.user.findFirst({
+      where: { email: email }
+    })
+    return findAccount && this.accountFactory.addFactory(findAccount)
   }
 }
