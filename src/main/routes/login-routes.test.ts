2559cb6f1325c42c15e7b235896a6dae76e35740
christiangsn <christianguimaraes1996@gmail.com>
2021-11-15 17:40:07 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -4,6 +4,7 @@ import app from '../app'
 import { Prisma } from '../../infra/database/Prisma/helpers/prismaHelpers'
 import { AccountPrismaRepository } from '../../infra/database/Prisma/Account/AccountPrismRepository'
 import { AddAccountFactory } from '../../domain/factories/addAccount'
+import { hash } from 'bcrypt'
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -15,9 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    await Prisma.client.user.updateMany({
-      data: {}
-    })
+    await Prisma.client.user.deleteMany({})
   })
 
   const addFactory = (): AddAccountFactory => {
@@ -38,6 +37,26 @@ describe('Login Routes', () => {
           email: 'christian@gmail.com',
           password: '123',
           passwordConfirm: '123'
+        })
+      expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should returns 200 on login', async () => {
+      const password = await hash('123', 12)
+      await Prisma.client.user.create({
+        data: {
+          name: 'Christian',
+          email: 'christian@gmail.com',
+          password: password
+        }
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'christian@gmail.com',
+          password: '123'
         })
       expect(200)
     })
