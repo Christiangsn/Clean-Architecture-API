93a3a77a13edcf02276893abcc05934a2832899d
christiangsn <christianguimaraes1996@gmail.com>
2021-11-15 16:45:33 -0300

    test: ensure AccountPrismaRepository updates the account accesToken on updateAccessToken Success

--- src/infra/database/Prisma/AccountRepository/Account.spec.ts
+++ src/infra/database/Prisma/AccountRepository/Account.spec.ts
@@ -60,4 +60,22 @@ describe('Account SqLite Repository', () => {
     const account = await sut.loadByEmail('any_email@email.com')
     expect(account).toBeFalsy()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const { id, accessToken } = await db.client.user.create({
+      data: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    })
+    expect(accessToken).toBeFalsy()
+    await sut.updateAccessToken(id, 'any_token')
+    const account = await db.client.user.findFirst({
+      where: { id: id }
+    })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
+  })
 })
