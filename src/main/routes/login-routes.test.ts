20cfc3c7c344d2b69816c251b59fbba3955e8bca
christiangsn <christianguimaraes1996@gmail.com>
2021-11-15 17:47:48 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -60,5 +60,15 @@ describe('Login Routes', () => {
         })
       expect(200)
     })
+
+    test('Should returns 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'christian@gmail.com',
+          password: '123'
+        })
+      expect(401)
+    })
   })
 })
