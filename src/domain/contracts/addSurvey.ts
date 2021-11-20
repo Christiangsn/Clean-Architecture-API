5890ec4913f3bcff50b165495f877d4caf3a2735
christiangsn <christianguimaraes1996@gmail.com>
2021-11-20 18:19:07 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

---
+++ src/domain/contracts/addSurvey.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-use-before-define */
+
+export interface AddSurveyModel {
+    question: string;
+    answers: SurveyAnswer[]
+}
+
+export interface SurveyAnswer {
+    image: string;
+    answer: string
+}
+
+export interface AddSurvey {
+    add (survey: AddSurveyModel): Promise<void>
+}
--- src/presentation/controllers/SurveyPaths/addSurvey/addSurveyController.ts
+++ src/presentation/controllers/SurveyPaths/addSurvey/addSurveyController.ts
@@ -1,17 +1,27 @@
+import { AddSurvey } from '@domain/contracts/addSurvey'
 import { badRequest } from '@presentation/helpers/http/httpHelper'
 import { HttpRequest, HttpResponse, ProtocolControllers } from '@presentation/protocol'
 import { IValidation } from '@presentation/protocol/validation'
 
 export class AddSurveyController implements ProtocolControllers {
   constructor (
-    private validation: IValidation
+    private validation: IValidation,
+    private addSurvey: AddSurvey
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { question, answers } = httpRequest.body
     const error = this.validation.validate(httpRequest.body)
+
     if (error) {
       return badRequest(error)
     }
-    return new Promise(resolve => resolve(null))
+
+    await this.addSurvey.add({
+      question: question,
+      answers: answers
+    })
+
+    return null
   }
 }
