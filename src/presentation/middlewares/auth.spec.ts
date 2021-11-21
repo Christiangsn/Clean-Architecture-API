3f58721c1ec374b8c43af5377e2e7e4d9aa49dea
christiangsn <christianguimaraes1996@gmail.com>
2021-11-21 00:05:14 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth.spec.ts
+++ src/presentation/middlewares/auth.spec.ts
@@ -1,11 +1,46 @@
+import { LoadAccountByToken } from '@domain/contracts/accountByToken'
+import { AccountModel } from '@domain/contracts/addAccount'
 import { IAccessDeniedError } from '@presentation/errors/accessDeniedError'
 import { forbidden } from '@presentation/helpers/http/httpHelper'
 import { AuthMiddleware } from './auth'
 
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'hash_password'
+})
+
 describe('Auth Middleware', () => {
   test('Should return 403 if no token exists in Authorization', async () => {
-    const sut = new AuthMiddleware()
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (accessToken: string, role?: string): Promise<AccountModel> {
+        return new Promise(resolve => resolve(makeFakeAccount()))
+      }
+    }
+
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new IAccessDeniedError()))
+  })
+
+  test('Should calls LoadAccountByToken with correct accessToken', async () => {
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (accessToken: string, role?: string): Promise<AccountModel> {
+        return new Promise(resolve => resolve(makeFakeAccount()))
+      }
+    }
+
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    await sut.handle({
+      headers: {
+        Authorization: 'Bearer any_token'
+      }
+    })
+
+    expect(loadSpy).toHaveBeenCalledWith('Bearer any_token')
   })
 })
