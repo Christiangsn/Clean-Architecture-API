e3a14956577cb3eed55e80ffc82de4e2529cd329
christiangsn <christianguimaraes1996@gmail.com>
2021-11-20 22:50:35 -0300

    test: ensure SurveyPrismaRepository  add a new survey on add success

---
+++ src/infra/database/Prisma/Survey/surveyPrismaRepository.spec.ts
@@ -0,0 +1,41 @@
+import { Prisma as db } from '../helpers/prismaHelpers'
+import { SurveyPrismaRepository } from './surveyPrismaRepository'
+
+describe('Account SqLite Repository', () => {
+  beforeAll(async () => {
+    await db.connection()
+  })
+
+  afterAll(async () => {
+    await db.disconnect()
+  })
+
+  beforeEach(async () => {
+    await db.client.surveyAnswers.deleteMany({})
+    await db.client.survey.deleteMany({})
+  })
+
+  const makeSut = (): SurveyPrismaRepository => {
+    return new SurveyPrismaRepository()
+  }
+
+  test('Should add an survey on success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      question: 'any_question',
+      answers: [{
+        image: 'any_image',
+        answer: 'any_anwer'
+      }, {
+        answer: 'other_anwer'
+      }]
+    })
+
+    const survey = await db.client.survey.findFirst({
+      where: {
+        question: 'any_question'
+      }
+    })
+    expect(survey).toBeTruthy()
+  })
+})
