8d20448f25b0574e5cc07d19e1ef297615795ada
christiangsn <christianguimaraes1996@gmail.com>
2021-11-21 01:21:15 -0300

    feat: ensure AutheMiddleware returns 500 if LoadAccountByToken throws

--- src/presentation/middlewares/auth.ts
+++ src/presentation/middlewares/auth.ts
@@ -2,7 +2,7 @@
 import { LoadAccountByToken } from '@domain/contracts/accountByToken'
 import { IAccessDeniedError } from '@presentation/errors'
 import { IInvalidTokenError } from '@presentation/errors/invalidTokenError'
-import { anauthorized, badRequest, forbidden, ok } from '@presentation/helpers/http/httpHelper'
+import { anauthorized, badRequest, forbidden, ok, serverError } from '@presentation/helpers/http/httpHelper'
 import { HttpRequest, HttpResponse } from '@presentation/protocol'
 import { ProtocolsMiddleware } from '../protocol/middleware'
 
@@ -14,30 +14,32 @@ export class AuthMiddleware implements ProtocolsMiddleware {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.['Authorization']
 
-    if (!accessToken) {
-      return forbidden(new IAccessDeniedError())
-    }
+    try {
+      if (!accessToken) {
+        return forbidden(new IAccessDeniedError())
+      }
 
-    const parts = accessToken.split(' ')
+      const parts = accessToken.split(' ')
 
-    if (parts.length !== 2) {
-      return anauthorized()
-    }
+      if (parts.length !== 2) {
+        return anauthorized()
+      }
 
-    const [scheme, token] = parts
+      const [scheme, token] = parts
 
-    if (!/^Bearer$/i.test(scheme)) {
-      return badRequest(new IInvalidTokenError())
-    }
+      if (!/^Bearer$/i.test(scheme)) {
+        return badRequest(new IInvalidTokenError())
+      }
 
-    if (accessToken) {
       const account = await this.loadAccountByToken.load(token)
 
       if (account) {
         return ok({ accountId: account.id })
       }
-    }
 
-    return forbidden(new IAccessDeniedError())
+      return forbidden(new IAccessDeniedError())
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
