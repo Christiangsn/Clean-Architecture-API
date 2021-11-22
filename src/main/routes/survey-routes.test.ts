3b418adad967147333659a3af73906ddd15ee49b
christiangsn <christianguimaraes1996@gmail.com>
2021-11-22 00:56:04 -0300

    test: ensure add survey returns 204 is valid acessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,6 +2,7 @@
 import request from 'supertest'
 import app from '../app'
 import { Prisma as db } from '../../infra/database/Prisma/helpers/prismaHelpers'
+import { sign } from 'jsonwebtoken'
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -13,6 +14,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
+    await db.client.user.deleteMany({})
     await db.client.surveyAnswers.deleteMany({})
     await db.client.survey.deleteMany({})
   })
@@ -32,6 +34,41 @@ describe('Login Routes', () => {
           }]
         })
       expect(403)
+    })
+
+    test('Should returns 403 on add survey with valid accessToken', async () => {
+      const res = await db.client.user.create({
+        data: {
+          name: 'Christian',
+          email: 'christian@gmail.com',
+          password: '123',
+          role: 'admin'
+        }
+      })
+      const { id } = res
+      const accessToken = sign({ id }, process.env.SECRET)
+      await db.client.user.update({
+        where: {
+          id: id
+        },
+        data: {
+          accessToken: accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({
+          email: 'Question',
+          answer: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 1'
+          }]
+        })
+      expect(204)
     })
   })
 })
