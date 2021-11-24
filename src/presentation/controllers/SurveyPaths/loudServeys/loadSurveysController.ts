e5309892b833d6fa1da8546c673081c84e60b8ac
christiangsn <christianguimaraes1996@gmail.com>
2021-11-24 00:20:59 -0300

    test: ensure LoadSurveysController returns 500 if LoadSurveysUseCase throws

--- src/presentation/controllers/SurveyPaths/loudServeys/loadSurveysController.ts
+++ src/presentation/controllers/SurveyPaths/loudServeys/loadSurveysController.ts
@@ -1,11 +1,15 @@
 import { LoadSurveys } from '@domain/contracts/loadSurveys'
-import { ok } from '@presentation/helpers/http/httpHelper'
+import { ok, serverError } from '@presentation/helpers/http/httpHelper'
 import { HttpRequest, HttpResponse, ProtocolControllers } from '@presentation/protocol'
 
 export class LoadSurveysController implements ProtocolControllers {
   constructor (private loadSurveys: LoadSurveys) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurveys.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurveys.load()
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
