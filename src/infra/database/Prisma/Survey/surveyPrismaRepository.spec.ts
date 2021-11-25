2ccb883841a9c01152c6807e0267b5b43d58fec7
christiangsn <christianguimaraes1996@gmail.com>
2021-11-25 17:00:03 -0300

    test: ensure PrismaReposiroty load all surveys

--- src/infra/database/Prisma/Survey/surveyPrismaRepository.spec.ts
+++ src/infra/database/Prisma/Survey/surveyPrismaRepository.spec.ts
@@ -1,5 +1,30 @@
+import { SurveysModel } from '@domain/contracts/loadSurveys'
 import { Prisma as db } from '../helpers/prismaHelpers'
 import { SurveyPrismaRepository } from './surveyPrismaRepository'
+
+const makeFakeSurveys = (): SurveysModel[] => {
+  return [{
+    id: 'any_id',
+    question: 'any_question',
+    createdAt: new Date(),
+    answers: [{
+      id: 'any_id',
+      image: 'any_image',
+      answer: 'any_answer',
+      createdAt: new Date()
+    }]
+  }, {
+    id: 'other_id',
+    question: 'other_question',
+    createdAt: new Date(),
+    answers: [{
+      id: 'other_id',
+      image: 'other_image',
+      answer: 'other_answer',
+      createdAt: new Date()
+    }]
+  }]
+}
 
 describe('Account SqLite Repository', () => {
   beforeAll(async () => {
@@ -41,7 +66,27 @@ describe('Account SqLite Repository', () => {
     })
   })
 
-  describe('Load()', () => {
+  describe('LoadAll()', () => {
+    test('Should load all surveys on success', async () => {
+      const surveysInsert = makeFakeSurveys()
 
+      for (const survey of surveysInsert) {
+        await db.client.survey.create({
+          data: {
+            question: survey.question,
+            answers: {
+              create: survey.answers
+            }
+
+          }
+        })
+      }
+
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+    })
   })
 })
